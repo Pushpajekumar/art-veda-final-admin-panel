a41426b3756a271ddab9aeccaa1cd7c0f6dbf504
PushpaJe kumar <mk6358522@gmail.com>
2025-04-16 10:49:12 +0530

    fix: add dynamic and revalidate exports to page component

--- app/admin/templates/page.tsx
+++ app/admin/templates/page.tsx
@@ -2,6 +2,9 @@ import Templates from "@/components/templates/templates";
 import TypeOfPost from "@/components/templates/type-of-post";
 import React from "react";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 const page = () => {
   return (
     <div>
